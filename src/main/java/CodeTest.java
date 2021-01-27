58091fb11ec8d857d4bcca0159a5562e8efe0d0e
Miguel Angel Fernandez <mianferi@gmail.com>
2021-01-27 20:01:22 +0100

    First step solution added

--- src/main/java/CodeTest.java
+++ src/main/java/CodeTest.java
@@ -1,13 +1,24 @@
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.function.Function;
 
 public class CodeTest {
     public static void main(String[] args) {
         System.out.println("Please replace this with calls to all completed tests.");
+        
+        System.out.println("STEP 1");
+        String[] myArray = new String[] {"1", "2", "3"};
+        String[] result = reverseArray(myArray);
+        System.out.println(Arrays.asList(result));
     }
 
     public static String[] reverseArray(String[] input) {
-        // add code here
-        return input;
+    	if (input != null) {
+        	Collections.reverse(Arrays.asList(input));
+        	return input;
+        } else {
+        	return new String[0];
+        }
     }
 
     public static String[] uppercaseArray(String[] input) {
